551f86e06c5a9c630732c372f073cde96b4038c9
Kariman95 <karimankaram889@gmail.com>
2021-10-27 23:32:39 +0200

    Update on InterComm_task2
    InterComm_task2 updated

--- Sprint12(RTOS)/Inter_Communication/InterComm_task2.c
+++ Sprint12(RTOS)/Inter_Communication/InterComm_task2.c
@@ -7,10 +7,13 @@ TaskHandle_t Task500Handler = NULL;
 
 /* create a semaphore */
 SemaphoreHandle_t xMutex;
+SemaphoreHandle_t xSemaphore1 = NULL;
 
 void COM_UARTTask2create(void)
 {
 	xMutex = xSemaphoreCreateMutex();
+	//xSemaphore1 = xSemaphoreCreateBinary();
+	//xSemaphoreGive( xSemaphore1 );
 	xTaskCreate(COM_UARTTask100, TASK100_NAME, TASK100_STACK_SIZE, NULL, TASK100_PRIORITY, &Task100Handler );
 	xTaskCreate(COM_UARTTask500, TASK500_NAME, TASK500_STACK_SIZE, NULL, TASK500_PRIORITY, &Task500Handler );
 }
@@ -18,16 +21,20 @@ void COM_UARTTask2create(void)
 void COM_UARTTask100( void * pvParameters )
 {
 	uint8_t u8Indx = 0;
+	uint32_t u32Indx2 = 0;
     for( ;; )
     {
-			if( xSemaphoreTake( xMutex, ( TickType_t ) 10 ) == pdTRUE )
-			{
+			xSemaphoreTake( xMutex, portMAX_DELAY );
+			
 				for(u8Indx=0; u8Indx<10; u8Indx++)
 				{
 						vSerialPutString("RTOS", 5);
+						/* Wait until string is transmitted. */
+						while(txDataSizeLeftToSend != 0);
 				}
+				xSerialPutChar('\n');
 				xSemaphoreGive( xMutex );
-			}
+			
 			vTaskDelay(100);
     }
 }
@@ -39,15 +46,17 @@ void COM_UARTTask500( void * pvParameters )
 		uint32_t u32Indx2 = 0;
     for( ;; )
     {
-			if( xSemaphoreTake( xMutex, ( TickType_t ) 10 ) == pdTRUE )
-			{
+			xSemaphoreTake( xMutex, portMAX_DELAY );
+			
 				for(u8Indx1=0; u8Indx1<10; u8Indx1++)
 				{
 						vSerialPutString("Hello", 6);
+						//while(txDataSizeLeftToSend != 0);
 						for(u32Indx2=0; u32Indx2<100000; u32Indx2++);
 				}
+				xSerialPutChar('\n');
 				xSemaphoreGive( xMutex );
-			}
+			
 			vTaskDelay(500);
 		}
 }
